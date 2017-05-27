7e1e7dddadd096f4295850a233da08bb6eafa878
zheng-jian <zj@zjdeMacBook-Pro.local>
2017-05-27 19:20:18 +0200

    update

--- APPLI/compression.c
+++ APPLI/compression.c
@@ -27,6 +27,7 @@ void codage(char *input, int taille){
 					temporaire=malloc(3*sizeof(char));
 					temporaire[0]=w[i];
 					temporaire[1]=w[i+1];
+					temporaire[2]='\0';
 					longeur=2;
 
 					//printf("chushihuade temporaire=%s\n",temporaire );
@@ -34,27 +35,32 @@ void codage(char *input, int taille){
 				else{
 					temporaire=malloc(2*sizeof(char));
 					temporaire[0]=w[i];
+					temporaire[1]='\0';
 					longeur=1;
 				}
 
 			}
 				else{
-				temporaire=malloc((strlen(wa)+2)*sizeof(char));
-				strcpy(temporaire,wa);
-			    strcat(temporaire,&w[i]);
-			    longeur=strlen(temporaire);
-
+					int p=strlen(wa);
+					temporaire=malloc((strlen(wa)+2)*sizeof(char));
+					for(int k=0;k<strlen(wa);k++){
+						temporaire[k]=wa[k];
+					}
+					temporaire[p]=w[i+1];
+					temporaire[p+1]='\0';
+					longeur=p+1;
 
 				}
 
 
 			int result=SequenceVersCode(temporaire,longeur);
 
+
 			if(result==-1 && longeur!=1 ){
 				int code=SequenceVersCode(wa,strlen(wa));
 				printf("%d  ",code);
 				//printf("%s\n",wa );
-				int q=Inserer(code,SequenceVersCode(&w[i+1],1));
+				int q=Inserer(code,SequenceVersCode(&temporaire[longeur-1],1));
 				//printf("%d\n",q );
 				free(wa);
 				wa=NULL;
@@ -65,7 +71,7 @@ void codage(char *input, int taille){
 
 
 			}
-			else if(result!=-1 && taille-1==i){
+			/*else if(result!=-1 && longeur==1){
 				int code=SequenceVersCode(temporaire,1);
 				printf("%d\n",code);
 				free(wa);
@@ -73,21 +79,33 @@ void codage(char *input, int taille){
 				free(temporaire);
 				temporaire=NULL;
 				break;
-			}
-			else if(result==-1 && longeur==1){
+			}*/
+			else if(i==taille-1 && longeur==1){
 				printf("%d\n",result);
+				break;
 			}
 			else{
 				free(wa);
 				wa=NULL;
-				wa=malloc(longeur*sizeof(char));
-				strcpy(wa,temporaire);
+				wa=malloc((longeur+1)*sizeof(char));
+				for(int k=0;k<longeur;k++){
+					wa[k]=temporaire[k];
+				}
+				wa[longeur]='\0';
 				if(taille-2==i){
+
+					free(temporaire);
+					temporaire=NULL;
+					free(wa);
+					wa=NULL;
 					printf("%d",result);
 					break;
 				}
-				free(temporaire);
-				temporaire=NULL;
+				else{
+					free(temporaire);
+					temporaire=NULL;
+				}
+
 
 			}
 
@@ -97,14 +115,14 @@ void codage(char *input, int taille){
 }
 
 int main(){
-	codage("abcabcabc",9);
+	codage("abcabcabcde",11);
 	int *taille;
 	printf("\n\n");
-/*	char *a=CodeVerChaine(258,taille);
-	char *b=CodeVerChaine(259,taille);
+	char *a=CodeVerChaine(263,taille);
+	//char *b=CodeVerChaine(259,taille);
 
 	printf("%s\n",a);
-	printf("%s\n",b);
-*/
+	//printf("%s\n",b);
+
 	return 0;
 }
